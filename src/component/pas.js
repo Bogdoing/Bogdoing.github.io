6c7a66b388b2b3f05c38ec54d29d0e0a9871de9a
Bogdoing <bogdoingg@gmail.com>
2022-11-14 22:04:41 +0300

    first commit

---
+++ src/component/pas.js
@@ -0,0 +1,22 @@
+function makeid() 
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 99; i++) {}
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+/*let func = (arg1, arg2, ...argN) => expression;*/
+
+var pass = "";
+//console.log(makeid());
+
+for (var i = 0; i < 10; i++){ 
+    pass += makeid();
+}
+console.log(pass);
+
+document.getElementById("demo").innerHTML = "<div>"+pass+"</div>";
+
