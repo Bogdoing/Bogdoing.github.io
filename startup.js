4af98c505ef6176eab644112b8e6a3a814310368
Bogdoing <bogdoingg@gmail.com>
2023-01-23 21:20:11 +0300

    Add vue folder

--- startup.js
+++ startup.js
@@ -1,3 +1,5 @@
-if ('serviceWorker' in navigator) { navigator.serviceWorker.register('/sw.js', { scope: '/' }); };
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' });
+};
 
 console.log('test startUP');
