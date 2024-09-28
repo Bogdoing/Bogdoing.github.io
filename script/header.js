86d810ca0a1208ba86c2b55684ce8df8adcddd49
Bogdoing <Bogdoingg@gmail.com>
2024-09-28 14:53:12 +0300

    add footer, icon. refactor header

--- script/header.js
+++ script/header.js
@@ -1,6 +1,7 @@
 /* Добавление по нажатию */
 function toggleMenu() {
-    const menuToggle = document.querySelector('.menuToggle');
+    // const menuToggle = document.querySelector('.menuToggle');
+    const menuToggle = document.querySelector('.manu-btn');
     const navigation = document.querySelector('.navigation');
     menuToggle.classList.toggle('active');
     navigation.classList.toggle('active');
